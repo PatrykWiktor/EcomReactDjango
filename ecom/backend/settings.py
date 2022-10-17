72c23feebe2bf132e1786faead6f751e09ce00a5
PatrykWiktor <114948096+PatrykWiktor@users.noreply.github.com>
2022-10-17 15:35:22 +0200

    Update settings.py

--- ecom/backend/settings.py
+++ ecom/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jdux+&0r4mjy6pu!-opo^ml6=ye9!ziv!((25(hu=h!bm48$=f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
